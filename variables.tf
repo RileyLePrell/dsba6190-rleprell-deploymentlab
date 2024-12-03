b0d602931b931d000ab818f49fede5f7ed2661c4
Riley LePrell <rileyleprell@gmail.com>
2024-12-02 21:44:00 -0500

    fixed naming issues

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "nleprell"
+  default     = "riley"
 }
 
 variable "class_name" {
